f073e10ec519a5655ceee703c1ebd8164922c05f
Ru <ru@beyondstays.com>
2020-02-06 01:33:04 -0800

    finished

--- Firewall.py
+++ Firewall.py
@@ -58,6 +58,8 @@ class Firewall:
             return False
         if type(port) != int:
             return False
+        if port < 1 or port > 65535:
+            return False
 
         # parsing the given ip (a string) into a tuple of integers
         ip = self.split_ip(ip)
